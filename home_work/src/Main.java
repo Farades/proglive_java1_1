1b3fdfc1fe4ab7eeb0e99781e0929a65e8d361ac
farades <creg2007@gmail.com>
2014-11-24 12:36:33 +0300

    init commit

---
+++ home_work/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String rogue = "Привет, разбойник номер ";
+        for (int i = 1; i < 41; i++) {
+            System.out.println(rogue + i);
+        }
+    }
+}
---
+++ package/Hello.java
@@ -0,0 +1,7 @@
+package ru.proglive.java1_1;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Java!");
+	}
+}
